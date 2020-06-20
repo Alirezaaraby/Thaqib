5c0b9fda8fc6346683b21200a28849f01cd63eaf
Alireza <alirezaaraby5@gmail.com>
2020-06-20 20:05:47 +0430

    Section Completed And Tabbed View Customized

--- app/src/main/java/app/thaqib/org/MainActivity.java
+++ app/src/main/java/app/thaqib/org/MainActivity.java
@@ -35,5 +35,6 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        viewPager.setCurrentItem(1, true);
     }
 }
