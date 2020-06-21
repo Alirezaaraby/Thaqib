d952c6cb502d0cb8cc1fc1ae34bfaa8294bdf728
Alireza <alirezaaraby5@gmail.com>
2020-06-21 06:31:05 +0430

    Events Completed

--- app/src/main/java/app/thaqib/org/Classes.java
+++ app/src/main/java/app/thaqib/org/Classes.java
@@ -4,16 +4,40 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.baoyz.widget.PullRefreshLayout;
+
 public class Classes extends Fragment {
+
+    ListView Classes_list;
+    PullRefreshLayout Refresh;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.classes_fragment, container, false);
+        Classes_list = (ListView) view.findViewById(R.id.classes_list);
+
+        Refresh = view.findViewById(R.id.classes_pull_to_refresh);
+
+        Refresh.setOnRefreshListener(new PullRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+                String[] values_refresh = {"Android","IPhone","WindowsMobile","Blackberry",
+                        "WebOS","Ubuntu","Windows7","Max OS X"};
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_spinner_dropdown_item, values_refresh);
+                Classes_list.setAdapter(adapter);
+                Refresh.setRefreshing(false);
+            }
+        });
+
         return view;
     }
 }
--- app/src/main/java/app/thaqib/org/News.java
+++ app/src/main/java/app/thaqib/org/News.java
@@ -4,16 +4,36 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.baoyz.widget.PullRefreshLayout;
+
 public class News extends Fragment {
+    ListView list;
+    PullRefreshLayout Refresh;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.news_fragment, container, false);
+        Refresh = view.findViewById(R.id.news_pull_to_refresh);
+        list = view.findViewById(R.id.news_list);
+
+        Refresh.setOnRefreshListener(new PullRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+                String[] values_refresh = {"Android","IPhone","WindowsMobile","Blackberry",
+                        "WebOS","Ubuntu","Windows7","Max OS X"};
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_spinner_dropdown_item, values_refresh);
+                list.setAdapter(adapter);
+                Refresh.setRefreshing(false);
+            }
+        });
         return view;
     }
 }
