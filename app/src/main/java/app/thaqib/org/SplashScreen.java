2af0108b9f6f54b7208f7e8ba6729da55ae510b4
Alireza <alirezaaraby5@gmail.com>
2020-06-21 06:45:04 +0430

    >lazyloader on splash screen completed

---
+++ app/src/main/java/app/thaqib/org/SplashScreen.java
@@ -0,0 +1,14 @@
+package app.thaqib.org;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
